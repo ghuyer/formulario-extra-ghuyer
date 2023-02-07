6aa3e572dc65ca8c3ce4c54f206d408917c8a56e
Guilherme Jorge Huyer <107578362+ghuyer@users.noreply.github.com>
2023-02-07 11:20:09 -0300

    Update code.js
    Adicionada a função mentoria

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
+function mentoria() {
+  if (checkTwo.checked) {
+    document.getElementById("showup").style.display = "block";
+  } else {
+    document.getElementById("showup").style.display = "none";
+  }
+}
+
+
 $('form input').attr('required', 'required');
